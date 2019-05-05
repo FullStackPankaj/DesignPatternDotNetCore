ddf4daad3a6cbc884a87d43c075e3bade07bb2e1
Srivastava, Pankaj <Pankaj.Srivastava@barco.com>
2019-05-05 21:21:16 +0530

    singleton patten non thread safe checkedn in

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SingletonNonThreadSafe;
 
 namespace DesignPattern
 {
@@ -6,7 +7,27 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program prog = new Program();
+            prog.CallNonThreadSafeClass();
+
+            Console.ReadKey();
+        }
+
+        private void CallNonThreadSafeClass()
+        {
+            SingletonNonThreadSafeClass s1 = SingletonNonThreadSafeClass.GetInstance();
+            SingletonNonThreadSafeClass s2 = SingletonNonThreadSafeClass.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+                SingletonNonThreadSafeClass.SomeBusinessLogic();
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
         }
     }
 }
---
+++ SingletonNonThreadSafe/SingletonNonThreadSafe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Singleton Design Pattern
+//
+// Intent: Lets you ensure that a class has only one instance, while providing a
+// Singleton Design Pattern
+//
+// Intent: Lets you ensure that a class has only one instance, while providing a
+// global access point to this instance.
+namespace SingletonNonThreadSafe
+{
+    // The Singleton class defines the `GetInstance` method that serves as an
+    // alternative to constructor and lets clients access the same instance of
+    // this class over and over.
+    public class SingletonNonThreadSafeClass
+    {
+        // The Singleton's instance is stored in a static field. There there are
+        // multiple ways to initialize this field, all of them have various pros
+        // and cons. In this example we'll show the simplest of these ways,
+        // which, however, doesn't work really well in multithreaded program.
+        private static SingletonNonThreadSafeClass _instance;
+
+        // The Singleton's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private SingletonNonThreadSafeClass()
+        {
+
+        }
+
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static SingletonNonThreadSafeClass GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new SingletonNonThreadSafeClass();
+            }
+
+            return _instance;
+        }
+
+        public static void SomeBusinessLogic()
+        {
+            Console.WriteLine("some business logic after single instance created");
+        }
+    }
+}
