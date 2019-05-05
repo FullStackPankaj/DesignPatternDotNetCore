53f4a44871f789c691320e86049c0f90d4df770d
Srivastava, Pankaj <Pankaj.Srivastava@barco.com>
2019-05-05 20:19:13 +0530

    code checkin

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
